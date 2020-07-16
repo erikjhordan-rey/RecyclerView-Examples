60c91f2cbd0f9f764a034e20459f4703e3b1ecf0
Erik Jhordan Rey <erikjhordan.rey@gmail.com>
2020-07-16 11:59:28 -0500

    Migrate base fragment

--- app/src/main/java/io/github/erikjhordanrey/recyclerview_sample/fragment/BaseFragment.java
+++ app/src/main/java/io/github/erikjhordanrey/recyclerview_sample/fragment/BaseFragment.java
@@ -4,77 +4,82 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.RecyclerView;
-
 import io.github.erikjhordanrey.recyclerview_sample.R;
-import java.util.ArrayList;
 import io.github.erikjhordanrey.recyclerview_sample.adapter.AdapterExample;
 import io.github.erikjhordanrey.recyclerview_sample.adapter.AdapterExampleTypes;
+import io.github.erikjhordanrey.recyclerview_sample.databinding.FragmentBaseBinding;
 import io.github.erikjhordanrey.recyclerview_sample.model.Picture;
 import io.github.erikjhordanrey.recyclerview_sample.presenter.PicturePresenter;
 import io.github.erikjhordanrey.recyclerview_sample.presenter.RecyclerItemClickListener;
 import io.github.erikjhordanrey.recyclerview_sample.view.PictureMvpView;
 import io.github.erikjhordanrey.recyclerview_sample.widget.ItemOffsetDecoration;
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import java.util.ArrayList;
 
 public abstract class BaseFragment extends Fragment implements PictureMvpView, RecyclerItemClickListener {
 
-    @BindView(R.id.recycler_view) RecyclerView recyclerView;
-    @BindView(R.id.progress_bar) ProgressBar progressBar;
     private PicturePresenter picturePresenter;
     RecyclerView.Adapter adapter;
 
-    @Nullable
-    @Override public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(getLayout(), container, false);
-        ButterKnife.bind(this, rootView);
+    private FragmentBaseBinding binding;
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentBaseBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         picturePresenter = new PicturePresenter();
         picturePresenter.attachedView(this);
         setupRecyclerView();
-
-        return rootView;
     }
 
-    @Override public void onResume() {
+    @Override
+    public void onResume() {
         super.onResume();
         picturePresenter.onResume();
     }
 
-    @Override public void setItems(ArrayList<Picture> pictureList) {
+    @Override
+    public void setItems(ArrayList<Picture> pictureList) {
         adapter = getAdapter(pictureList);
-        recyclerView.setAdapter(adapter);
+        binding.recyclerView.setAdapter(adapter);
 
-        if(adapter instanceof AdapterExample)
+        if (adapter instanceof AdapterExample)
             ((AdapterExample) adapter).setRecyclerItemClickListener(this);
-        else if(adapter instanceof AdapterExampleTypes)
+        else if (adapter instanceof AdapterExampleTypes)
             ((AdapterExampleTypes) adapter).setRecyclerItemClickListener(this);
 
     }
 
-    @Override public void showProgress() {
-        progressBar.setVisibility(View.VISIBLE);
-        recyclerView.setVisibility(View.INVISIBLE);
+    @Override
+    public void showProgress() {
+        binding.progressBar.setVisibility(View.VISIBLE);
+        binding.recyclerView.setVisibility(View.INVISIBLE);
     }
 
-    @Override public void hideProgress() {
-        progressBar.setVisibility(View.INVISIBLE);
-        recyclerView.setVisibility(View.VISIBLE);
+    @Override
+    public void hideProgress() {
+        binding.progressBar.setVisibility(View.INVISIBLE);
+        binding.recyclerView.setVisibility(View.VISIBLE);
     }
 
-    @Override public void showMessage(String message) {
+    @Override
+    public void showMessage(String message) {
         Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
     }
 
-    @Override public void onDestroy() {
+    @Override
+    public void onDestroy() {
         picturePresenter.detachView();
         super.onDestroy();
     }
@@ -86,11 +91,11 @@ public abstract class BaseFragment extends Fragment implements PictureMvpView, R
 
     private void setupRecyclerView() {
 
-        if(getLayoutManager() != null)
-            recyclerView.setLayoutManager(getLayoutManager());
+        if (getLayoutManager() != null)
+            binding.recyclerView.setLayoutManager(getLayoutManager());
 
-        recyclerView.addItemDecoration(new ItemOffsetDecoration(recyclerView.getContext(), R.dimen.item_decoration));
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        binding.recyclerView.addItemDecoration(new ItemOffsetDecoration(binding.recyclerView.getContext(), R.dimen.item_decoration));
+        binding.recyclerView.setItemAnimator(new DefaultItemAnimator());
 
     }
 
